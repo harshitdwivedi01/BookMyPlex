c4731394754414e99b27b0da47e48b4693c23752
Harshit Dwivedi <77053371+harshitdwivedi01@users.noreply.github.com>
2023-02-23 19:01:26 +0530

    Incomplete Home Page

---
+++ pages/home/Home.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import Header from "../../Components/navbar/header/Header";
+import Navbar from "../../Components/navbar/Navbar";
+import "./home.css";
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+        </div>
+    )
+}
+
+export default Home
