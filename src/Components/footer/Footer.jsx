df5959313cda172bcd2878c39486649fbfaaaa5c
Harshit Dwivedi <77053371+harshitdwivedi01@users.noreply.github.com>
2023-02-26 18:38:56 +0530

    The Footer

---
+++ src/Components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="fLists">
+                <ul className="fLi">
+                    <li className="fListItem">Countries</li>
+                    <li className="fListItem">Regions</li>
+                    <li className="fListItem">Cities</li>
+                    <li className="fListItem">Districts</li>
+                    <li className="fListItem">Airports</li>
+                    <li className="fListItem">Hotels</li>
+                </ul>
+                <ul className="fLi">
+                    <li className="fListItem">Homes</li>
+                    <li className="fListItem">Apartments</li>
+                    <li className="fListItem">Resorts</li>
+                    <li className="fListItem">Villas</li>
+                    <li className="fListItem">Hostels</li>
+                    <li className="fListItem">Guest Houses</li>
+                </ul>
+                <ul className="fLi">
+                    <li className="fListItem">Car Rental</li>
+                    <li className="fListItem">Flight Finder</li>
+                    <li className="fListItem">Restaurant reservations</li>
+                    <li className="fListItem">All destinations</li>
+                    <li className="fListItem">Traveller Review Awards</li>
+                    <li className="fListItem">B&Bs</li>
+                </ul>
+                <ul className="fLi">
+                    <li className="fListItem">Unique Places to Stay</li>
+                    <li className="fListItem">Discover</li>
+                    <li className="fListItem">Review</li>
+                    <li className="fListItem">Holidays Deals</li>
+                    <li className="fListItem">Careers</li>
+                    <li className="fListItem">Press Center</li>
+                </ul>
+                <ul className="fLi">
+                    <li className="fListItem">Customer Help</li>
+                    <li className="fListItem">Investor Relations</li>
+                    <li className="fListItem">Partner Dispute</li>
+                    <li className="fListItem">Terms & Conditions</li>
+                    <li className="fListItem">Privacy Policy</li>
+                    <li className="fListItem">Corporate Contact</li>
+                </ul>
+            </div>
+            <div className="fText">Copyright © 2023 BookMyPlex™</div>
+        </div>
+    )
+}
+
+export default Footer
