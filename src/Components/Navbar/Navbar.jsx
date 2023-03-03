28c19c404915a81cb54099bf0297c8498f92005d
Rishabh <70380751+SlayerRishabh@users.noreply.github.com>
2023-03-03 16:00:15 +0530

    Updated Navbar for Authentication
    Using Auth0 in Navbar for Login & Logout functionality.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import "./navbar.css"
+// {/*Importing Auth0 for login */}
+import { useAuth0 } from "@auth0/auth0-react"; 
+
 
 const Navbar = () => {
+    const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+
     return (
         <div className="navbar">
             <div className="navcontainer">
                 <span className="logo">BookMyPlex</span>
                 <div className="navItems">
-                    <button className="navButton">Register</button>
-                    <button className="navButton">Login</button>
+
+                    {/* {isAuthenticated && <p> {user.name} </p>} */}   {/*For showing user name */}
+
+                    {/* Showing login & logout btn based on user login status */}
+                    {isAuthenticated ? (
+                        <button className="navButton" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+                        ) : (
+                            <button className="navButton" onClick={() => loginWithRedirect()}>My Account</button>
+                        )
+                    }
+
                 </div>
             </div>
         </div>
